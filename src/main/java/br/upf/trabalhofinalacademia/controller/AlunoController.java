0b5797423f08ce1e476fcfae4eebb81efc8260d4
Pauloohg <188387@upf.br>
2026-06-11 11:14:14 -0300

    Adicionando o converter de Personal e Aluno

--- src/main/java/br/upf/trabalhofinalacademia/controller/AlunoController.java
+++ src/main/java/br/upf/trabalhofinalacademia/controller/AlunoController.java
@@ -10,7 +10,10 @@ import jakarta.ejb.EJBException;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.FacesConverter;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +55,50 @@ public class AlunoController implements Serializable {
 
     public void setSelected(AlunoEntity selected) {
         this.selected = selected;
+    }
+    
+    public AlunoEntity getAluno(java.lang.Integer id) {
+    return ejbFacade.find(id);
+    }
+
+    @FacesConverter(forClass = AlunoEntity.class)
+    public static class AlunoControllerConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            AlunoController controller
+                    = (AlunoController) facesContext.getApplication().getELResolver()
+                            .getValue(facesContext.getELContext(), null, "alunoController");
+            return controller.getAluno(getKey(value));
+        }
+
+        java.lang.Integer getKey(String value) {
+            java.lang.Integer key;
+            key = Integer.valueOf(value);
+            return key;
+        }
+
+        String getStringKey(java.lang.Integer value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof AlunoEntity) {
+                AlunoEntity o = (AlunoEntity) object;
+                return getStringKey(o.getId());
+            } else {
+                return null;
+            }
+        }
     }
         
     public static enum PersistAction {
--- src/main/java/br/upf/trabalhofinalacademia/controller/PersonalController.java
+++ src/main/java/br/upf/trabalhofinalacademia/controller/PersonalController.java
@@ -10,7 +10,10 @@ import jakarta.ejb.EJBException;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.convert.FacesConverter;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +55,50 @@ public class PersonalController implements Serializable {
 
     public void setSelected(PersonalEntity selected) {
         this.selected = selected;
+    }
+    
+    public PersonalEntity getPersonal(java.lang.Integer id) {
+    return ejbFacade.find(id);
+    }
+
+    @FacesConverter(forClass = PersonalEntity.class)
+    public static class PersonalControllerConverter implements Converter {
+
+        @Override
+        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
+            if (value == null || value.length() == 0) {
+                return null;
+            }
+            PersonalController controller
+                    = (PersonalController) facesContext.getApplication().getELResolver()
+                            .getValue(facesContext.getELContext(), null, "personalController");
+            return controller.getPersonal(getKey(value));
+        }
+
+        java.lang.Integer getKey(String value) {
+            java.lang.Integer key;
+            key = Integer.valueOf(value);
+            return key;
+        }
+
+        String getStringKey(java.lang.Integer value) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(value);
+            return sb.toString();
+        }
+
+        @Override
+        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+            if (object == null) {
+                return null;
+            }
+            if (object instanceof PersonalEntity) {
+                PersonalEntity o = (PersonalEntity) object;
+                return getStringKey(o.getId());
+            } else {
+                return null;
+            }
+        }
     }
     
     public static enum PersistAction {
